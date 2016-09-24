57b6a5615848fd55b6d5b7b2239f301b23d18afb
Kelvin Lee <kiyolee@hotmail.com>
2016-09-24 10:17:57 +1000

    Identified this version is from FreeBSD 8.2.

--- md5/getopt/getopt.c
+++ md5/getopt/getopt.c
@@ -12,10 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
  * 4. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
@@ -40,7 +36,7 @@ static char sccsid[] = "@(#)getopt.c	8.3 (Berkeley) 4/27/95";
 #include <sys/cdefs.h>
 #endif
 #ifdef __FreeBSD__
-__FBSDID("$FreeBSD: src/lib/libc/stdlib/getopt.c,v 1.7 2004/03/06 17:05:45 ache Exp $");
+__FBSDID("$FreeBSD: release/8.2.0/lib/libc/stdlib/getopt.c 165903 2007-01-09 00:28:16Z imp $");
 #endif
 
 #ifdef __FreeBSD__
