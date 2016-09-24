29dbd6153f8b69e469d082902da781c995b923e3
Kelvin Lee <kiyolee@gmail.com>
2016-09-25 01:04:03 +1000

    Updating from FreeBSD 10.3.

--- libmd/supp/w32defs.h
+++ libmd/supp/w32defs.h
@@ -1,8 +1,13 @@
 #ifndef W32DEFS_H
 #define W32DEFS_H
 
+#if defined(_MSC_VER) && _MSC_VER >= 1600
+#include <stdint.h>
+#else
 typedef unsigned short uint16_t;
-typedef unsigned long uint32_t;
+typedef unsigned int uint32_t;
+typedef unsigned __int64 uint64_t;
+#endif
 
 typedef uint16_t u_int16_t;
 typedef uint32_t u_int32_t;
