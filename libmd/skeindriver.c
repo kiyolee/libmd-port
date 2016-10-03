d79ae574147bef3d11fff2ed078185954d65a956
Kelvin Lee <kelvin.lee@lansa.com.au>
2016-10-04 08:45:19 +1100

    Fixed for Linux and IBM i.

--- libmd/skeindriver.c
+++ libmd/skeindriver.c
@@ -11,14 +11,22 @@
  * These notices must be retained in any copies of any part of this
  * documentation and/or software. */
 
+#ifdef unix
 #include <sys/cdefs.h>
+#endif
+#ifdef __FreeBSD__
 __FBSDID("$FreeBSD: release/11.0.0/lib/libmd/skeindriver.c 300921 2016-05-29 01:15:36Z allanjude $");
+#endif
 
 #include <sys/types.h>
 
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+
+#if defined(__OS400__)
+#include "supp/e2a.h"
+#endif
 
 #include "skein.h"
 
@@ -44,9 +52,15 @@ static void
 SKEINString(char *string)
 {
 	char buf[2*128 + 1];
-
+#ifdef __OS400__
+	char asc[1024];
+	e2acpy(asc, string);
+	SKEIN_Data(asc, strlen(string), buf);
+#else
+	SKEIN_Data(string, strlen(string), buf);
+#endif
 	printf("SKEIN%d (\"%s\") = %s\n",
-	       SKEIN, string, SKEIN_Data(string, strlen(string), buf));
+	       SKEIN, string, buf);
 }
 
 /* Digests a reference suite of strings and prints the results. */
@@ -66,3 +80,6 @@ main(void)
 
 	return 0;
 }
+
+/* vim: ts=4:sts=4:sw=4:noet
+ */
